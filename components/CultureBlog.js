5ee55f7904a8a5697fe9f939aed5fd249980b5b7
Ryan Booth <ryanbooth@chiedo.com>
2019-02-22 15:14:03 -0500

    responsive-design changes

--- components/CultureBlog.js
+++ components/CultureBlog.js
@@ -22,12 +22,13 @@ class CultureBlog extends React.Component {
         <>
         <Container id="CultureBlog" fluid >
         <Row>
-          <Col id="featured-image-ricardo" className="greybox" lg={4}><img src='https://labs.chiedo.com/wp-content/themes/chiedolabs/img/wp-landing-page/img-ricardo-monicle.jpg'/></Col>
-            <Col className="startUp" lg={5} style={{ padding: '3em' }}>
+          <Col md={6} lg={4}className="greybox" >
+          <img id="featured-image-ricardo"  src='https://labs.chiedo.com/wp-content/themes/chiedolabs/img/wp-landing-page/img-ricardo-monicle.jpg'/></Col>
+            <Col md={6} lg={5}className="startUp" style={{ padding: '3em' }}>
               <h3 style={{ fontSize: '2rem' }}>Start-up Culture, Corporate-Grade Service.</h3>
               <p style={{ fontSize: '1rem' }}>We create an atmosphere that fosters creativity and professionalism. In this way, we allow our team to hone your vision with our digital strategy and build realities that will take your business to the next level.</p>
               <button className="btn">Get a quote</button></Col>
-            <Col lg={3} id="PostColumn" style={{ fontSize: '2.5rem', textAlign: 'center' }}>
+            <Col md={12} lg={3} id="PostColumn" style={{ fontSize: '2.5rem', textAlign: 'center' }}>
               <h3><span className="redText">FEATURED</span> POST</h3>
               <a className="greybox" href={this.state.currentBlogLink}><div id="featuredPostLink" >{this.state.currentBlogTitle}</div></a>
             </Col>
