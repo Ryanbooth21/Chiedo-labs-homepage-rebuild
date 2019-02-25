f7d2ea4a0db7978a273a926469b778fe3b236c5a
Ryan Booth <ryanbooth@chiedo.com>
2019-02-25 17:00:02 -0500

    portfolio items

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -16,68 +16,65 @@ const Portfolio = (props) => {
         {console.log(editedArray)}
         <Container id="case-study-container justify-content-center" fluid style={{ lineHeight: '32px', width: '100vw' }}>
         <Row className="case-study-row" style={{ width: '100vw' }}>
-            <Col lg={2}>
+            
                 <div style={{ backgroundImage: `url(${editedArray[0].source_url})`}} className="case-study-div">
                     <h4 className="case-study-title">{editedArray[0].alt_text}</h4>
                     
                 </div>
-            </Col>
-            <Col lg={2}>
+            
+            
                 <div style={{ backgroundImage: `url(${editedArray[1].source_url})`}} className="case-study-div">
                     <h4 className="case-study-title">{editedArray[1].alt_text}</h4>
                     
                 </div>
-            </Col>
-            <Col lg={2}>
+            
+            
                 <div style={{ backgroundImage: `url(${editedArray[2].source_url})`}} className="case-study-div">
                     <h4 style={{ backgroundImage: `url(${editedArray[2].source_url})`}} className="case-study-title">{editedArray[2].alt_text}</h4>
                     
                 </div>
-            </Col>
-            <Col lg={2}>
+            
+            
                 <div style={{ backgroundImage: `url(${editedArray[3].source_url})`}} className="case-study-div">
                     <h4 className="case-study-title">{editedArray[3].alt_text}</h4>
                     
                 </div>
-            </Col>
-            <Col lg={2}>
+            
+            
                 <div style={{ backgroundImage: `url(${editedArray[4].source_url})`}} className="case-study-div">
                     <h4 className="case-study-title">{editedArray[4].alt_text}</h4>
                     
                 </div>
-            </Col>
+            
         </Row>
         <Row className="case-study-row " style={{ width: '100vw' }}>
-            <Col lg={2}>
+          
                 <div style={{ backgroundImage: `url(${editedArray[5].source_url})`}} className="case-study-div">
                     <h4 className="case-study-title">Husel</h4>
                    
                 </div>
-            </Col>
-            <Col lg={2}>
+           
+           
                 <div style={{ backgroundImage: `url(${editedArray[6].source_url})`}} className="case-study-div">
                     <h4 className="case-study-title">{editedArray[6].alt_text}</h4>
                     
                 </div>
-            </Col>
-            <Col lg={2}>
+            
                 <div style={{ backgroundImage: `url(${editedArray[7].source_url})`}} className="case-study-div">
                     <h4 className="case-study-title">{editedArray[7].alt_text}</h4>
                    
                 </div>
-            </Col>
-            <Col lg={2}>
+
                 <div style={{ backgroundImage: `url(${editedArray[8].source_url})`}} className="case-study-div">
                     <h4 className="case-study-title">{editedArray[8].alt_text}</h4>
                     
                 </div>
-            </Col>
-            <Col lg={2}>
+            
                 <div style={{ backgroundImage: `url(${editedArray[9].source_url})`}} className="case-study-div">
                     <h4 className="case-study-title">{editedArray[9].alt_text}</h4>
                     
                 </div>
-            </Col>
+            
         </Row>
       </Container>
       <style jsx>{` 
